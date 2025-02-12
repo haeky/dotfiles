2daaa2762af13fd0c62e788ee6c43bcb9f4ed561
Laurent Dang <dang.laurent@gmail.com>
2025-02-11 22:09:44 -0500

    Fallback to Copilot when not using OpenAI proxy

--- config/nvim/lua/plugins/avante.lua
+++ config/nvim/lua/plugins/avante.lua
@@ -5,23 +5,28 @@ return {
       "stevearc/dressing.nvim",
       "nvim-lua/plenary.nvim",
       "MunifTanjim/nui.nvim",
+      "zbirenbaum/copilot.lua",
     },
+    build = "make",
     config = function()
-      local openai = require("avante.providers.openai")
+      local opts = { provider = "copilot" }
+      local has_openai_proxy_details = vim.fn.filereadable(vim.fn.expand("~/.oai-proxy-details"))
 
-      require("avante").setup({
-        -- @type AvanteProvider
-        provider = "shopify-ai",
-        vendors = {
-          ["shopify-ai"] = {
+      if has_openai_proxy_details == 1 then
+        local openai = require("avante.providers.openai")
+        opts.provider = "shopify"
+        opts.vendors = {
+          ["shopify"] = {
             endpoint = "https://proxy.shopify.ai/v3/v1",
             model = "anthropic:claude-3-5-sonnet",
             api_key_name = "cmd:openai_key cat",
             parse_curl_args = openai.parse_curl_args,
             parse_response_data = openai.parse_response,
           },
-        },
-      })
+        }
+      end
+
+      require("avante").setup(opts)
     end,
   },
 }
